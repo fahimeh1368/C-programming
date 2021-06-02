e9472338232a0214f1603a7b5b3e8dac03d6a97f
Fahimeh Hajari <fahime.hajari@gmail.com>
2021-06-02 07:52:55 -0400

    Indexer added to project

---
+++ NewFeatures/NewFeatures/HttpCookie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewFeatures
+{
+    class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictionary;
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+    }
+}
---
+++ NewFeatures/NewFeatures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "fahimeh";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
